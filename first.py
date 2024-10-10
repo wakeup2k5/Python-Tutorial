1623a5e8291a50bcf8298847e7f887ff43362052
wakeup2k5 <jamesacollins@hotmail.com>
2024-10-10 15:00:48 -0400

    first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+# write a program that prints an introduction to you (include your name, age, and a summary of what you want to learn in this project)
+
+firstname = 'James'
+lastname = 'Collins'
+age = 43
+objective = 'Learn about the python programming language and it''s uses in AI'
+
+print("First Name: " + firstname)
+print("Last Name: " + lastname)
+print("Age: " + str(age) + " years old")
+print("Objective: " + objective)
