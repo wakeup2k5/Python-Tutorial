bd9afb21ab44f5ed3a049873e4f90ca1b318fb1e
wakeup2k5 <jamesacollins@hotmail.com>
2024-10-15 14:54:49 -0400

    Fibonacci sequence

--- Module Assessments/Fourth Module Assessment/main.py
+++ Module Assessments/Fourth Module Assessment/main.py
@@ -1,4 +1,4 @@
-# Calculate the - given the inputs from the user. 
+# Calculate the area and circumference of a circle given the inputs from the user. 
 # You are given the required radius input from the user. 
 # Print out the area and circumference of the circle.
 
@@ -9,18 +9,6 @@ radius = float(input("Please enter the radius of the circle (cm): "))
 circumference = 2 * pi * radius
 area = pi * (radius ** 2)
 
-ls=[2,8,6,5,2,7,9]   
+print(f"The circumference is: {circumference} cm")
+print(f"The area is: {area} square cm")
 
-    ## Type your code here 
-
-index = -1
-
-for ind in range(len(ls)):
-    if ls[ind] % 2 != 0:
-        index = ind
-        break
-
-#####################  
-
-
-print(index)
