87bf3e92137f0e05c902ffb907c20f37829d1037
wakeup2k5 <jamesacollins@hotmail.com>
2024-10-25 15:46:57 -0400

    recursive fibonacci function

--- Module Assessments/Twelfth Module Assessment/main.py
+++ Module Assessments/Twelfth Module Assessment/main.py
@@ -1,14 +1,18 @@
 # Write the Fibonacci function you wrote in the last submodule, but this time with recursion.
 
-def fibonacci_sequence(max_value, num1=0, num2=1, sequence=None):
+def fibonacci_sequence(length, sequence=None):
     if sequence is None:
-        sequence = [num1]
-    
-    if num2 > max_value:
+        if length > 1:
+            sequence = [0, 1] 
+        else: 
+            sequence = [0] 
+
+    if len(sequence) >= length:
         return sequence
     else:
-        sequence.append(num2)
-        return fibonacci_sequence(max_value, num2, num1 + num2, sequence)
+        sequence.append(sequence[-1] + sequence[-2])
+        return fibonacci_sequence(length, sequence)
     
-max_value = 100
-print(fibonacci_sequence(max_value))
+
+quantityRequested = int(input("Please enter the number of fibonacci numbers to display: "))
+print("Fibonacci sequence of length", quantityRequested, ":", fibonacci_sequence(quantityRequested))
