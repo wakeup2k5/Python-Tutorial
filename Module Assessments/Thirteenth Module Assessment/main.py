d2e1d22812d98b0f9ce94e12c2ab13b624984c18
wakeup2k5 <jamesacollins@hotmail.com>
2024-10-25 15:58:24 -0400

    average of the numbers in a list

--- Module Assessments/Thirteenth Module Assessment/main.py
+++ Module Assessments/Thirteenth Module Assessment/main.py
@@ -1,8 +1,11 @@
 # Write a function to calculate the average of the numbers in a list of items using the built-in sum function
 
 def calculate_average(arr):
-    retVal = sum(arr) / len(arr)
-    return retVal
+    if len(arr) > 0:
+        retVal = sum(arr) / len(arr)
+        return retVal
+    else:
+        return 0
 
 randomNumbers = [ 555, 8, 3, 0, 1, 7, 5, 2, 9, 4, 6 , 11, 200, 4, 2]
 print(calculate_average(randomNumbers))
