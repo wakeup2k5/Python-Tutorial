05061b6ab5cb256a56d22ac194b2f92b43c3f690
wakeup2k5 <jamesacollins@hotmail.com>
2024-10-29 10:22:32 -0400

    library catalogue system

--- Module Assessments/Eighteenth Module Assessment/main.py
+++ Module Assessments/Eighteenth Module Assessment/main.py
@@ -1,53 +1,89 @@
-# Create a simple student database system that stores information about students, such as their names, ages, and grades.
-# Use classes to represent students and include methods for displaying student information and calculating average grades.
+# Build a library catalogue system that manages information about books, including their title, author, and availability status. 
+# Use classes to represent books and include methods for checking out and returning books.
+import os
+clear = lambda: os.system('cls')
 
-class Student:
-    def __init__(self, name, age, grades):
-        self.name = name
-        self.age = age
-        self.grades = grades
+class Book:
+    def __init__(self, title, author, status):
+        self.title = title
+        self.author = author
+        self.status = status
+        
+    def checkout(self):
+        self.status = "checked out"
+        print(self.title + " checked out successfully")
 
-    def displayGrades(self):
-        for key in self.grades:
-            print(key + ": " + str(self.grades[key]))
-
-    def displayStudentAverage(self):
-        countOfGrades = len(self.grades)
-        sumOfGrades = 0
-        for key in self.grades:
-            sumOfGrades += int(self.grades[key])
-        averageOfGrades = sumOfGrades / countOfGrades
-        print("Grades Average: " + str(averageOfGrades))  
-
-    def displayStudentInformation(self):
-        print("Name: " + self.name)
-        print("Age: " + str(self.age))
-        self.displayGrades()
-        self.displayStudentAverage()
-
-student1Grades = {"Math":90, "Physics": 85, "English": 65}
-student1 = Student("James Collins", 21, student1Grades)
-
-student2Grades = {"Math":95, "Physics": 80, "English": 75}
-student2 = Student("Jane Doe", 20, student2Grades)
-
-students = [student1]
-students.append(student2)
-
-addAnotherStudent = input("Would you like to add another student? (Y/N): ")
-if(addAnotherStudent == "Y"):
-    anotherStudentName = input("Enter the Students Name: ")
-    anotherStudentAge = input("Enter the Students Age: ")
-    anotherStudentMathGrade = input("Enter the Students Math Grade: ")
-    anotherStudentPhysicsGrade = input("Enter the Students Physics Grade: ")
-    anotherStudentEnglishGrade = input("Enter the Students English Grade: ")
-    anotherStudent = Student(anotherStudentName, anotherStudentAge, {"Math":anotherStudentMathGrade, "Physics": anotherStudentPhysicsGrade, "English": anotherStudentEnglishGrade})
-    students.append(anotherStudent)
-
-for student in students:
-    print("---------------")
-    student.displayStudentInformation()
-
-
+    def checkin(self):
+        self.status = "available"
+        print(self.title + " checked in successfully")
     
+class Catalogue:
+    books = []
+    
+    def __init__(self):
+        pass
 
+    def addBook(self, book):
+        self.books.append(book)
+        
+    def listBooks(self):
+        for idx in range(len(self.books)):
+            print(str(idx + 1) + " : " + self.books[idx].title + " by " + self.books[idx].author + " | STATUS: " + self.books[idx].status)
+
+def main():
+    clear()
+    catalogue = Catalogue()
+    catalogue.addBook(Book("The Road", "Cormac McCarthy", "available"))
+    catalogue.addBook(Book("Never Let Me Go", "Kazuo Ishiguro", "available"))
+    catalogue.addBook(Book("White Teeth", "Zadie Smith", "available"))
+    catalogue.addBook(Book("Normal People", "Sally Rooney", "available"))
+    catalogue.addBook(Book("The Night Circus", "Erin Morgenstern", "available"))
+
+    menu_options = {
+        "1": "View Entire Catalogue", 
+        "2": "Check out book", 
+        "3": "Check in book", 
+        "4": "Exit", 
+        }
+
+    print("Book Catalogue")
+    
+    exitProgram = False
+
+    while exitProgram == False:
+        clear()
+        print("MAIN MENU")
+        print("---")
+        for key in menu_options:
+            print(key,":", menu_options[key])
+        print("---")
+        selection = input("Please choose a menu number from above: ")
+        if selection == "1":
+            clear()
+            catalogue.listBooks()
+            input("\nPress Enter to return to main menu...")
+        if selection == "2":
+            clear()
+            catalogue.listBooks()
+            chosenBook = int(input("\nEnter a book number to check out: "))
+            if(chosenBook <= len(catalogue.books) and chosenBook > 0):
+                chosenBookIndex = chosenBook - 1
+                catalogue.books[chosenBookIndex].checkout()
+            else:
+                print("\nInvalid selection.  Check out failed.")
+            input("\nPress Enter to return to main menu...")
+        if selection == "3":
+            clear()
+            catalogue.listBooks()
+            chosenBook = int(input("\nEnter a book number to check in: "))
+            if(chosenBook <= len(catalogue.books) and chosenBook > 0):
+                chosenBookIndex = chosenBook - 1
+                catalogue.books[chosenBookIndex].checkin()
+            else:
+                print("\nInvalid selection.  Check in failed.")
+            input("\nPress Enter to return to main menu...")
+        if selection == "4":
+            clear()
+            break
+
+main()
